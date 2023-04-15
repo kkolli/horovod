075e532916084e5f26fba74965a94b9b32e11a55
dyin <dyin@nuro.ai>
2023-04-15 10:53:53 -0700

    make build python version agnostic

--- horovod/common/util.py
+++ horovod/common/util.py
@@ -32,6 +32,7 @@ EXTENSIONS = ['tensorflow', 'torch', 'mxnet']
 
 def get_ext_suffix():
     """Determine library extension for various versions of Python."""
+    """
     ext_suffix = sysconfig.get_config_var('EXT_SUFFIX')
     if ext_suffix:
         return ext_suffix
@@ -39,7 +40,7 @@ def get_ext_suffix():
     ext_suffix = sysconfig.get_config_var('SO')
     if ext_suffix:
         return ext_suffix
-
+    """
     return '.so'
 
 
